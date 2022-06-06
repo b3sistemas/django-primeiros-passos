07dbc09e264b6b2b7cf37e1d1c3609732a214d04
nicanor.silva <nicanor.silva@g.globo>
2022-06-05 23:37:53 -0300

    Criação do app teste

--- TestePrj/settings.py
+++ TestePrj/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'TestePrjApp1'
+    'TestePrjMainApp'
 ]
 
 MIDDLEWARE = [
